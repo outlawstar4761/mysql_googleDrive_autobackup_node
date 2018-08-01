537555cad231592b63c25db6ea7bfe1f627f20e6
outlawstar4761 <outlawstar4761@gmail.com>
2018-08-01 15:39:30 -0500

    Add files via upload

---
+++ googleModule.js
@@ -0,0 +1,67 @@
+var googleModule = (function(){
+    const fs = require('fs');
+    const readline = require('readline');
+    const {google} = require('googleapis');
+    const mime = require('mime-types');
+    const pathinfo = require('pathinfo');
+    const SCOPES = [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/drive',
+    ];
+    const TOKEN_PATH = 'token.json';
+    const CRED_PATH = 'credentials.json';
+
+    function authorize(credentials,callback){
+        const {client_secret,client_id,redirect_uris} = credentials.installed;
+        const oAuth2Client = new google.auth.OAuth2(client_id,client_secret,redirect_uris[0]);
+        fs.readFile(TOKEN_PATH,(err,token)=>{
+            if(err) return getAccessToken(oAuth2Client,callback);
+            oAuth2Client.setCredentials(JSON.parse(token));
+            callback(oAuth2Client);
+        });
+    }
+    function getAccessToken(oAuth2Client,callback){
+        const authUrl = oAuth2Client.generateAuthUrl({access_type:'offline',scope:SCOPES});
+        console.log('Authorize this app by visiting this url: ',authUrl);
+        const rl = readline.createInterface({input:process.stdin,output:process.stdout});
+        rl.question('Enter the code from that page here: ',(code)=>{
+            rl.close();
+            oAuth2Client.getToken(code,(err,token)=>{
+                if(err) return callback(err);
+                oAuth2Client.setCredentials(token);
+                fs.writeFile(TOKEN_PATH,JSON.stringify(token),(err)=>{
+                    if(err) console.error(err);
+                    console.log('Token stored to',TOKEN_PATH);
+                });
+                callback(oAuth2Client);
+            });
+        });
+    }
+    return {
+        CRED_PATH:CRED_PATH,
+        authorize:function(credentials,callback){
+            authorize(credentials,callback);
+        },
+        getFileList:function(auth){
+            return new Promise((resolve,reject)=>{
+                var fileList = [];
+                const drive = google.drive({version:'v3',auth});
+                drive.files.list({pageSize:10,fields:'nextPageToken,files(id,name)'},(err,res)=>{
+                    if(err) reject(err);
+                    const files = res.data.files;
+                    files.forEach((file)=>{fileList.push(file);});
+                    resolve(fileList);
+                });
+            });
+        },
+        uploadFile:async function(auth,filePath){
+            const drive = google.drive({version:'v3',auth});
+            var fileInfo = pathinfo(filePath);
+            var fileMetaData = {name:fileInfo.filename};
+            var media = {mimeType:mime.lookup(fileInfo.extname),body:fs.createReadStream(filePath)}
+            const res = await drive.files.create({resource:fileMetaData,media:media});
+        }
+    }
+}());
+
+module.exports = googleModule;
---
+++ index.js
@@ -0,0 +1,42 @@
+const google = require('./googleModule');
+const { exec } = require('child_process');
+const DBFILE = 'databases.json';
+const PASSPHRASE = '1234';
+
+
+function execBackUpScript(database,passphrase){
+    return new Promise((resolve,reject)=>{
+        var outputFile = database + '.sql.gpg';
+        var cmd = './mysqlbackup.sh ' + database + ' ' + passphrase;
+        exec(cmd,(err,stdout,stderr)=>{
+            if(err) reject(err);
+            resolve(outputFile);
+        });
+    });
+}
+
+function getDatabases(){
+    return new Promise((resolve,reject)=>{
+        fs.readFile(DBFILE,(err,content)=>{
+            if (err) reject(err);
+            resolve(JSON.parse(content));
+        });
+    });
+}
+
+fs.readFile(google.CRED_PATH,(err,content)=>{
+    if (err) return console.log('Error loading client secret file:',err);
+    getDatabases().then((databases)=>{
+        databases.forEach((database)=>{
+            execBackUpScript(database,PASSPHRASE).then((outputFile)=>{
+                google.authorize(JSON.parse(content),(auth)=>{google.uploadFile(auth,outputFile).catch(console.error)});
+            },(err)=>{
+                return console.log('Error Executing Backup:',err);
+            });
+        });
+    },(err)=>{
+        return console.log('Error Reading Databases:',err);
+    });
+    // google.authorize(JSON.parse(content),(auth)=>{google.getFileList(auth).then(console.log,console.error);});
+    //google.authorize(JSON.parse(content),(auth)=>{google.uploadFile(auth,'testFile.png').catch(console.error)});
+});
