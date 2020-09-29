d43b426149619fddd38f574bf690a8fbcb61397c
John Watson <outlawstar4761@gmail.com>
2020-09-29 15:26:04 -0500

    cleaning up

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const google = require('./googleModule');
 const {exec} = require('child_process');
+const path = require('path');
+const fs = require('fs');
 const DBFILE = __dirname + '/databases.json';
+const BACKPATH = __dirname + "/"
 const PASSPHRASE = '1234';
 var parentFolders = ['1BWiXZKWmbidk2RbQVecL8du6Ma2RigtZ'];
 
@@ -11,6 +14,7 @@ function execBackUpScript(database,passphrase){
     var cmd = __dirname + '/mysqlbackup.sh ' + database + ' ' + passphrase;
     exec(cmd,(err,stdout,stderr)=>{
       if(err) reject(err);
+      if(stderr) console.error(stderr);
       resolve(outputFile);
     });
   });
@@ -35,18 +39,30 @@ function pruneOldBackUps(auth){
 function cleanup(){
   exec('rm ' + __dirname + "/*.gpg",(err,stdout,stderr)=>{
     if(err) throw err
+    if(stderr) console.error(stderr);
   });
 }
-
+function backupsExist(){
+  let fileCount = fs.readdirSync(BACKPATH);
+  fileCount.forEach((file)=>{
+    if(path.extname(BACKPATH + file) == '.gpg'){
+      return true;
+    }
+  });
+}
+//path.extname()
+/*THE ACTION TAKES PLACE HERE*/
+if(backupsExist()){
+  cleanup()
+}
 let secret = JSON.parse(fs.readFileSync(google.CRED_PATH));
 let databases = JSON.parse(fs.readFileSync(DBFILE));
 google.authorize(secret,(auth)=>{
   pruneOldBackUps(auth);
   databases.forEach((database)=>{
     execBackUpScript(database,PASSPHRASE).then((outputFile)=>{
-      var fileMetaData = {name:outputFile,parents:parentFolders};
+      var fileMetaData = {name:path.basename(outputFile),parents:parentFolders};
       google.uploadFile(auth,outputFile,fileMetaData).catch(console.error)
     }).catch(console.error);
   });
-  cleanup()
 });
