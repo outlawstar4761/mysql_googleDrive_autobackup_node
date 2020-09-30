3d57079660dac7f2e442a0abd3cf7426f7451dca
John Watson <outlawstar4761@gmail.com>
2020-09-30 16:22:35 -0500

    forgot to await

--- samples/backup_and_encrypt_await.js
+++ samples/backup_and_encrypt_await.js
@@ -4,8 +4,8 @@ const targetDb = 'LOE';
 (async ()=>{
   sqlmod.setUser('root','sample');
   try{
-    sqlmod.backupDB(targetDb);
-    sqlmod.encryptOutput(sqlmod.getOutPath(targetDb),'sample');
+    await sqlmod.backupDB(targetDb);
+    await sqlmod.encryptOutput(sqlmod.getOutPath(targetDb),'sample');
   }catch(err){
     console.error(err);
   }
