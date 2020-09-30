29a6451e058dced8c62aa608a6a77fab94908955
John Watson <outlawstar4761@gmail.com>
2020-09-30 16:28:30 -0500

    abstracting variables

--- samples/backup_and_encrypt_await.js
+++ samples/backup_and_encrypt_await.js
@@ -1,11 +1,14 @@
 const sqlmod = require('../src/mysqlModule');
-const targetDb = 'LOE';
+const mysqluser = 'root';
+const mysqlpass = 'sample';
+const targetDb = 'sample';
+const passphrase = 'sample';
 
 (async ()=>{
-  sqlmod.setUser('root','sample');
+  sqlmod.setUser(mysqluser,mysqlpass);
   try{
     await sqlmod.backupDB(targetDb);
-    await sqlmod.encryptOutput(sqlmod.getOutPath(targetDb),'sample');
+    await sqlmod.encryptOutput(sqlmod.getOutPath(targetDb),passphrase);
   }catch(err){
     console.error(err);
   }
