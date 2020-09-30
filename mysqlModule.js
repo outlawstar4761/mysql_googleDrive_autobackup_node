1f7c0eabb5b1d6c08edb1b8d37d8ac6d19416d4d
outlawstar4761@gmail.com <outlawstar4761@gmail.com>
2020-09-30 14:33:20 -0500

    abstracting

---
+++ mysqlModule.js
@@ -0,0 +1,49 @@
+var mysqlModule = (()=>{
+  const {exec} = require('child_process');
+  const THISDIR = __dirname + '/';
+
+  let _user = '';
+  let _pass = '';
+
+  function _execShellCmd(cmd){
+    return new Promise((resolve,reject)=>{
+      exec(cmd,(err,stdout,stderr)=>{
+        if(err) reject(err);
+        if(stderr) reject(stderr);
+        resolve(stdout);
+      });
+    });
+  }
+  function _deleteFile(absolutePath){
+    let cmd = 'rm ' + absolutePath;
+    return _execShellCmd(cmd);
+  }
+  function _backupDB(dbName){
+    let outFile = THISDIR + dbName + '.sql';
+    let cmd = 'mysqldump --user=' + user + ' --password=' + pass + ' ' + dbName + ' > ' + outFile;
+    return _execShellCmd(cmd);
+  }
+  function _encryptOutput(absolutePath,passphrase){
+    let cmd = 'gpg -c --batch --passphrase=' + passphrase + ' ' + absolutePath;
+    return _execShellCmd(cmd);
+  }
+  return {
+    user:_user,
+    pass:_pass,
+    setUser:function(user,pass){
+      _user = user;
+      _pass = pass;
+      this.user = _user;
+      this.pass = _pass;
+    },
+    backupDB:function(dbname){
+      if(this.user == '' || this.pass == ''){
+        throw new Error('Unable to execute before setting username and password');
+      }
+      return _backupDB(dbName);
+    },
+    encryptOutput:function(absolutePath,passphrase){
+      return _encryptOutput(absolutePath,passphrase);
+    }
+  }
+});
