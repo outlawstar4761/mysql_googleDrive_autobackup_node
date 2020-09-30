3730b70b5bee4feae8311fcf3888a33b12c4f80d
outlawstar4761@gmail.com <outlawstar4761@gmail.com>
2020-09-30 18:38:45 -0500

    new sample

---
+++ samples/download_and_rollback_await.js
@@ -0,0 +1,39 @@
+const sqlmod = require('../src/mysqlModule');
+const google = require('../src/googleModule');
+const fs = require('fs');
+
+const mysqluser = 'root';
+const mysqlpass = 'sample';
+const targetDb = 'sample';
+const passphrase = 'sample';
+
+async function _doDownload(googleAuth,targetDb){
+  let dest = sqlmod.getEncryptedPath(targetDb);
+  try{
+    await google.downloadFile();
+  }catch(err){
+    console.error(err);
+  }
+}
+async function _getDownloadId(googleAuth,targetDb){
+  let files = await google.getFileList();
+  for(file in files){
+    if(files[file].name.match(targetDb)){
+      return files[file].id;
+    }
+  }
+  return null;
+}
+
+(async ()=>{
+  const secret = await JSON.parse(fs.readFile(google.CRED_PATH));
+  sqlmod.setUser(mysqluser,mysqlpass);
+  google.authorize(secret,(auth)=>{
+    let targetId = _getDownloadId(auth,targetDb);
+    try{
+      await google.downloadFile(auth,targetId,sqlmod.getEncryptedPath(targetDb));
+    }catch(err){
+      console.error(err);
+    }
+  });
+})();
