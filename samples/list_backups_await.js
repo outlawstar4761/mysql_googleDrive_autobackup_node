87e77778a7dc28aff9845f006df0531941da5c37
John Watson <outlawstar4761@gmail.com>
2020-10-01 12:24:16 -0500

    test

--- samples/list_backups_await.js
+++ samples/list_backups_await.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const google = require('../src/googleModule');
 let secret = google.getSecret();
-let targetFolder = '1BWiXZKWmbidk2RbQVecL8du6Ma2RigtZ';
+let options = {q:"name contains '.sql.gpg'"}
 
 (async ()=>{
   google.authorize(secret,async (auth)=>{
-    let fileList = await google.getFileList(auth,{q:"name contains '.sql.gpg'"}).catch(console.error);
+    let fileList = await google.getFileList(auth,options).catch(console.error);
     console.log(fileList);
   });
 })();
