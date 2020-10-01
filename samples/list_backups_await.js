9c82ba3f81a55198e65725285b39d8021267fcd2
John Watson <outlawstar4761@gmail.com>
2020-10-01 12:21:43 -0500

    file search

--- samples/list_backups_await.js
+++ samples/list_backups_await.js
@@ -5,7 +5,7 @@ let targetFolder = '1BWiXZKWmbidk2RbQVecL8du6Ma2RigtZ';
 
 (async ()=>{
   google.authorize(secret,async (auth)=>{
-    let fileList = await google.getFileList(auth,{driveId:});
+    let fileList = await google.getFileList(auth,{q:"name contains '.sql.gpg'"}).catch(console.error);
     console.log(fileList);
   });
 })();
