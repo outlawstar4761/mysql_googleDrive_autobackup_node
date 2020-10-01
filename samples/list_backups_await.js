eabd73f1aec64cd4ee4c51e5b989e971b8d791c8
John Watson <outlawstar4761@gmail.com>
2020-10-01 11:53:41 -0500

    error

--- samples/list_backups_await.js
+++ samples/list_backups_await.js
@@ -4,7 +4,7 @@ let secret = google.getSecret();
 
 (async ()=>{
   google.authorize(secret,async (auth)=>{
-    let fileList = await google.getFileList();
+    let fileList = await google.getFileList(auth,{});
     console.log(fileList);
   });
 })();
