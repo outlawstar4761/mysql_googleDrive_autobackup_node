10dbb9e1b38d25e4017b6ac73089b83ef21925c5
John Watson <outlawstar4761@gmail.com>
2020-10-01 11:38:05 -0500

    await

--- samples/list_backups_await.js
+++ samples/list_backups_await.js
@@ -4,7 +4,7 @@ let secret = google.getSecret();
 
 (async ()=>{
   google.authorize(secret,(auth)=>{
-    let fileList = google.getFileList();
+    let fileList = await google.getFileList();
     console.log(fileList);
   });
 })();
