7a4a78a94977a9e8a390db4271546b632affdb60
John Watson <outlawstar4761@gmail.com>
2020-10-08 11:30:20 -0500

    missing requirment

--- src/googleModule.js
+++ src/googleModule.js
@@ -3,7 +3,7 @@ var googleModule = (function(){
     const readline = require('readline');
     const {google} = require('googleapis');
     const mime = require('mime-types');
-    //const pathinfo = require('pathinfo');
+    const path = require('path');
     const SCOPES = [
         'https://www.googleapis.com/auth/userinfo.profile',
         'https://www.googleapis.com/auth/drive',
